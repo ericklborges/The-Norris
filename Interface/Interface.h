0ab02a1f54fb6b02b4c9102e37951451f23a99e4
Erick Borges <ericklborges@gmail.com>
2021-02-10 19:23:58 -0300

    Add interface module

---
+++ Interface/Interface.h
@@ -0,0 +1,18 @@
+//
+//  Interface.h
+//  Interface
+//
+//  Created by erick.lozano.borges on 10/02/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Interface.
+FOUNDATION_EXPORT double InterfaceVersionNumber;
+
+//! Project version string for Interface.
+FOUNDATION_EXPORT const unsigned char InterfaceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Interface/PublicHeader.h>
+
+
