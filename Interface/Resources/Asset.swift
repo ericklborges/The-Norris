5add7da68adddb985cb8d0e0d8d1163df6f2c239
Erick Borges <ericklborges@gmail.com>
2021-02-10 20:46:10 -0300

    Add Asset enum

---
+++ Interface/Resources/Asset.swift
@@ -0,0 +1,17 @@
+//
+//  Asset.swift
+//  Interface
+//
+//  Created by erick.lozano.borges on 10/02/21.
+//
+
+import UIKit
+
+public enum Asset {
+    public static let arrowUpBackward = UIImage(systemName: "arrow.up.backward")!
+    public static let clockArrowCirclepath = UIImage(systemName: "clock.arrow.circlepath")!
+    public static let magnifyingglass = UIImage(systemName: "magnifyingglass")!
+    public static let pencilSlash = UIImage(systemName: "pencil.slash")!
+    public static let squareAndArrowUp = UIImage(systemName: "square.and.arrow.up")!
+    public static let wifiSlash = UIImage(systemName: "wifi.slash")!
+}
