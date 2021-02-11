1518b9a386f03879f368e6e3dd4d26b67f802d2f
Erick Borges <ericklborges@gmail.com>
2021-02-11 18:48:14 -0300

    Remove unused metod from TagView

---
+++ Interface/Component/SectionHeaderView/SectionHeaderView.swift
@@ -0,0 +1,8 @@
+//
+//  SectionHeaderView.swift
+//  Interface
+//
+//  Created by erick.lozano.borges on 11/02/21.
+//
+
+import Foundation
--- Interface/Component/TagView/TagView.swift
+++ Interface/Component/TagView/TagView.swift
@@ -35,11 +35,6 @@ public class TagView: UIView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    // MARK: - Setup
-    private func setup() {
-        
-    }
 }
 
 // MARK: - Auto Layout
