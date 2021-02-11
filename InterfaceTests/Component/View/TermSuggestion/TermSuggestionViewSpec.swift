ff001be6d185998567182ebe4ea597deaeb6dddd
Erick Borges <ericklborges@gmail.com>
2021-02-11 20:06:08 -0300

    [closes #21] Add TermSuggestionView

--- InterfaceTests/Component/View/TermSuggestion/TermSuggestionViewSpec.swift
+++ InterfaceTests/Component/View/TermSuggestion/TermSuggestionViewSpec.swift
@@ -5,4 +5,48 @@
 //  Created by erick.lozano.borges on 11/02/21.
 //
 
-import Foundation
+import Quick
+import Nimble
+import Nimble_Snapshots
+
+@testable import Interface
+
+class TermSuggestionViewSpec: QuickSpec {
+    override func spec() {
+        
+        describe("TermSuggestionView") {
+            
+            var sut: TermSuggestionView!
+
+            context("when initialized") {
+                beforeEach {
+                    sut = TermSuggestionView()
+                    sut.text = "Term Suggestion"
+                    sut.setDynamicSize(forHeight: 44.0)
+                }
+                
+                context("and offset separator is set") {
+                    beforeEach {
+                        sut.setOffsetSeparator()
+                    }
+                    
+                    it("shuold layout itself properly") {
+                        expect(sut) == snapshot("TermSuggestionView_Layout_Offset_Separator")
+                    }
+                }
+                
+                context("and full width separator is set") {
+                    beforeEach {
+                        sut.setFullWidthSeparator()
+                    }
+                    
+                    it("shuold layout itself properly") {
+                        expect(sut) == snapshot("TermSuggestionView_Layout_Full_Width_Separator")
+                    }
+                }
+            }
+        }
+    }
+}
+
+
