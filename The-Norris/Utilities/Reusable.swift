c7e8018fd28b951790fe2eed33ab330d84fa22e7
Erick Borges <ericklborges@gmail.com>
2021-02-14 19:33:10 -0300

    Add Reusable protocol support
    - UITableViewCell
    - UITableView.register(_:)
    - UITableView.dequeueReusableCell(for:)
    
    - UICollectionViewCell
    - UITableView.register(_:)
    - UITableView.dequeueReusableCell(for:)

---
+++ The-Norris/Utilities/Reusable.swift
@@ -0,0 +1,53 @@
+//
+//  Reusable.swift
+//  The-Norris
+//
+//  Created by erick.lozano.borges on 14/02/21.
+//
+
+import UIKit
+
+protocol Reusable: AnyObject {
+    static var reuseIdentifier: String { get }
+}
+
+extension Reusable {
+    static var reuseIdentifier: String {
+        return String(describing: self)
+    }
+}
+
+// MARK: - UITableView
+
+extension UITableViewCell: Reusable {}
+
+extension UITableView {
+    func register<T: UITableViewCell>(_: T.Type) {
+        register(T.self, forCellReuseIdentifier: T.reuseIdentifier)
+    }
+    
+    func dequeueReusableCell<T: UITableViewCell>(for indexPath: IndexPath) -> T {
+        guard let cell = dequeueReusableCell(withIdentifier: T.reuseIdentifier, for: indexPath) as? T else {
+            return T()
+        }
+        return cell
+    }
+}
+
+// MARK: - UICollectionView
+
+extension UICollectionViewCell: Reusable {}
+
+extension UICollectionView {
+    func register<T: UICollectionViewCell>(_: T.Type) {
+        register(T.self, forCellWithReuseIdentifier: T.reuseIdentifier)
+    }
+    
+    func dequeueReusableCell<T: UICollectionViewCell>(for indexPath: IndexPath) -> T {
+        guard let cell = dequeueReusableCell(withReuseIdentifier: T.reuseIdentifier, for: indexPath) as? T else {
+            return T()
+        }
+        return cell
+    }
+}
+
