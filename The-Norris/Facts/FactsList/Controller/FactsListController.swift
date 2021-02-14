2c126bd6a20ec9bd80097feb79198f652eb25cca
Erick Borges <ericklborges@gmail.com>
2021-02-14 19:02:00 -0300

    Add FactsListController

---
+++ The-Norris/Facts/FactsList/Controller/FactsListController.swift
@@ -0,0 +1,38 @@
+//
+//  FactsListController.swift
+//  The-Norris
+//
+//  Created by erick.lozano.borges on 14/02/21.
+//
+
+import UIKit
+
+class FactsListController: UIViewController {
+    
+    // MARK: - Views
+    private lazy var searchButton: UIBarButtonItem = {
+        return UIBarButtonItem(
+            barButtonSystemItem: .search,
+            target: self,
+            action: #selector(searchButtonTap)
+        )
+    }()
+    
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupNavigation()
+    }
+    
+    // MARK: - Setup
+    func setupNavigation() {
+        title = "Facts"
+        navigationItem.largeTitleDisplayMode = .always
+        navigationItem.rightBarButtonItem = searchButton
+    }
+    
+    // MARK: - Actions
+    @objc
+    private func searchButtonTap() {
+    }
+}
