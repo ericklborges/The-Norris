d30537f0d0d30a8a7c60b14f7913c95ea3e567f6
Erick Borges <ericklborges@gmail.com>
2021-02-15 15:21:02 -0300

    Turn FactsListViewController into StateViewController and integrate it with ViewModel

--- The-Norris/Facts/FactsList/Controller/FactsListViewController.swift
+++ The-Norris/Facts/FactsList/Controller/FactsListViewController.swift
@@ -6,8 +6,9 @@
 //
 
 import UIKit
+import Interface
 
-class FactsListViewController: UIViewController {
+class FactsListViewController: StateViewController {
     
     // MARK: - Views
     private let factsView = FactsListView()
@@ -20,10 +21,24 @@ class FactsListViewController: UIViewController {
         )
     }()
     
+    // MARK: - Properties
+    private let viewModel: FactsListViewModel
+    
     // MARK: - Life Cycle
+    init(viewModel: FactsListViewModel = FactsListViewModel()) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigation()
+        viewModel.delegate = self
     }
     
     override func loadView() {
@@ -40,4 +55,58 @@ class FactsListViewController: UIViewController {
     // MARK: - Actions
     @objc
     private func searchButtonTap() { }
+}
+
+// MARK: - FactsListViewModelDelegate
+
+extension FactsListViewController: FactsListViewModelDelegate {
+    func didChange(facts: [Fact]) {
+        factsView.setup(facts: facts)
+        state = .main
+    }
+    
+    func didStartRequest() {
+        setLoadingState()
+    }
+    
+    func didReceiveError() {
+        setErrorState()
+    }
+    
+    func didReceiveEmptyResult() {
+        seteEmptyState()
+    }
+}
+
+// MARK: - Presentation State
+
+private extension FactsListViewController {
+    func setLoadingState() {
+        state = .loading(title: "Thinking", message: "Norris brain is processing your request, it won't take long.")
+    }
+    
+    func setErrorState() {
+        let buttonConfiguration = ButtonConfiguration(title: "Try Again", action: { [weak self] in
+            self?.searchButtonTap()
+        })
+        
+        state = .error(
+            image: Symbol.clockArrowCirclepath.image(pointSize: 48),
+            title: "Something wrong",
+            message: "Norris brain could not process yout request, try again later.",
+            buttonConfiguration: buttonConfiguration
+        )
+    }
+    
+    func seteEmptyState() {
+        let buttonConfiguration = ButtonConfiguration(title: "Try Again", action: { [weak self] in
+            self?.searchButtonTap()
+        })
+        
+        state = .empty(
+            title: "Nothing found",
+            message: "Unfortunatly Norris did not create a word for that. Yet.",
+            buttonConfiguration: buttonConfiguration
+        )
+    }
 }
