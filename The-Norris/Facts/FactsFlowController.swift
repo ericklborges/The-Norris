d025e02336799fd07cdc6bc3927319612df8f1fc
Erick Borges <ericklborges@gmail.com>
2021-02-15 18:07:56 -0300

    Add Conformance to FlowDelegates on FactsFlowController

--- The-Norris/Facts/FactsFlowController.swift
+++ The-Norris/Facts/FactsFlowController.swift
@@ -10,12 +10,53 @@ import Interface
 
 class FactsFlowController: UIViewController {
     
+    // MARK: - Parameters
     private let navigation = UINavigationController()
     
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupNavigation()
+        showFirstController()
+    }
+    
+    // MARK: - Setup
+    private func setupNavigation() {
         navigation.applyCustomAppearence()
-        navigation.show(FactsListViewController(), sender: nil)
         install(child: navigation)
+    }
+}
+
+// MARK: - Navigation
+
+extension FactsFlowController {
+    private func showFirstController() {
+        let controller = FactsListViewController()
+        controller.flowDelegate = self
+        searchEndAction = controller.fetchFacts(query:)
+        navigation.show(controller, sender: nil)
+    }
+    
+    private func showSearchController() {
+        let controller = FactsSearchViewController()
+        let navigation = UINavigationController(rootViewController: controller)
+        controller.flowDelegate = self
+        present(navigation, animated: true, completion: nil)
+    }
+}
+
+// MARK: - FactsListViewControllerFlowDelegate
+
+extension FactsFlowController: FactsListViewControllerFlowDelegate {
+    func factsListShowsSearch(_ controller: FactsListViewController) {
+        showSearchController()
+    }
+}
+
+// MARK: - FactsSearchViewControllerFlowDelegate
+
+extension FactsFlowController: FactsSearchViewControllerFlowDelegate {
+    func factsSearch(_ controller: FactsSearchViewController, didEndWith query: String) {
+        controller.dismiss(animated: true, completion: nil)
     }
 }
