68df8647c867f83530c243256f9a784edcf3851e
Erick Borges <ericklborges@gmail.com>
2021-02-15 14:32:23 -0300

    Add JsonHelper and ClientMock

---
+++ The-NorrisTests/Utilities/JsonHelper.swift
@@ -0,0 +1,22 @@
+//
+//  JsonHelper.swift
+//  The-NorrisTests
+//
+//  Created by erick.lozano.borges on 15/02/21.
+//
+
+import Foundation
+
+class JsonHelper {
+    static func getDataFrom<T: Decodable>(json file: String) -> T? {
+        guard let path = Bundle(for: JsonHelper.self).path(forResource: file, ofType: "json") else { return nil }
+        
+        do {
+            let data = try Data(contentsOf: URL(fileURLWithPath: path), options: [])
+            let object = try JSONDecoder().decode(T.self, from: data)
+            return object
+        } catch {
+            return nil
+        }
+    }
+}
