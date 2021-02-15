f9ed7476e652b0b39f26b96f43f9765b8ef6a6f2
Erick Borges <ericklborges@gmail.com>
2021-02-15 18:43:46 -0300

    Test FactsSearchViewController

---
+++ The-NorrisTests/Facts/FactsSearch/Controller/FactsSearchViewControllerSpec.swift
@@ -0,0 +1,52 @@
+//
+//  FactsSearchViewControllerSpec.swift
+//  The-NorrisTests
+//
+//  Created by erick.lozano.borges on 15/02/21.
+//
+
+import Quick
+import Nimble
+import Nimble_Snapshots
+
+@testable import The_Norris
+
+class FactsSearchViewControllerSpec: QuickSpec {
+    override func spec() {
+        describe("FactsSearchViewController") {
+            
+            var sut: FactsSearchViewController!
+            var navigationMock: UINavigationController!
+            var flowDelegateSpy: FactsSearchViewControllerFlowDelegateSpy!
+            
+            context("when initialized") {
+                
+                beforeEach {
+                    sut = FactsSearchViewController()
+                    
+                    flowDelegateSpy = FactsSearchViewControllerFlowDelegateSpy()
+                    sut.flowDelegate = flowDelegateSpy
+                    
+                    navigationMock = UINavigationController(rootViewController: sut)
+                    navigationMock.navigationBar.backgroundColor = .white
+                    WindowHelper.showInTestWindow(navigationMock)
+                }
+                
+                it("should layout it's navigation properly") {
+                    expect(sut.navigationController?.navigationBar) == snapshot("FactsSearchViewController_Navigation_Layout")
+                }
+                
+                context("and textFieldShouldReturn(_:) is caled") {
+                    beforeEach {
+                        _ = sut.textFieldShouldReturn(UITextField())
+                    }
+                    
+                    it("should call flowDelegate's factsSearch(_:didEndWith:) method") {
+                        expect(flowDelegateSpy.calledFactsSearchDidEndWith) == true
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ The-NorrisTests/Facts/FactsSearch/Spy/FactsSearchViewControllerFlowDelegateSpy.swift
@@ -0,0 +1,17 @@
+//
+//  FactsSearchViewControllerFlowDelegateSpy.swift
+//  The-NorrisTests
+//
+//  Created by erick.lozano.borges on 15/02/21.
+//
+
+@testable import The_Norris
+
+class FactsSearchViewControllerFlowDelegateSpy: FactsSearchViewControllerFlowDelegate {
+    
+    private(set) var calledFactsSearchDidEndWith: Bool = false
+    
+    func factsSearch(_ controller: FactsSearchViewController, didEndWith query: String) {
+        calledFactsSearchDidEndWith = true
+    }
+}
