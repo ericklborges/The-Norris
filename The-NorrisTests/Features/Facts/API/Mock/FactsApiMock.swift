6874250d24b1cf30bb25ad91947bb8a41a2c9c49
Erick Borges <ericklborges@gmail.com>
2021-02-16 16:37:52 -0300

    Update Tests folder organization

--- The-NorrisTests/Features/Facts/API/Mock/FactsApiMock.swift
+++ The-NorrisTests/Features/Facts/API/Mock/FactsApiMock.swift
@@ -13,10 +13,21 @@ class FactsApiMock: FactsApiProtocol {
     
     var shouldFail: Bool = false
     var fetchFactsReturn: FactsQuery = .stub()
+    var fetchCategoriesReturn: [String] = []
+    
+    private let error = ClientError(reason: .api("FactsApiMock should sFail"), statusCode: 99)
+    
+    func fetchCategories(completion: @escaping (Result<[String], ClientError>) -> Void) {
+        if shouldFail {
+            completion(.failure(error))
+        } else {
+            completion(.success(fetchCategoriesReturn))
+        }
+    }
     
     func fetchFacts(query: String, completion: @escaping (Result<FactsQuery, ClientError>) -> Void) {
         if shouldFail {
-            completion(.failure(ClientError(reason: .api("FactsApiMock should sFail"), statusCode: 99)))
+            completion(.failure(error))
         } else {
             completion(.success(fetchFactsReturn))
         }
