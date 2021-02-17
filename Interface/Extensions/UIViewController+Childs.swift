861ab3a11cfa47ddd9aededfce5aa6189ee049fe
Erick Borges <ericklborges@gmail.com>
2021-02-17 20:12:31 -0300

    Fix layout update thread issues

--- Interface/Extensions/UIViewController+Childs.swift
+++ Interface/Extensions/UIViewController+Childs.swift
@@ -10,35 +10,26 @@ import UIKit
 public extension UIViewController {
     
     func switchChild(to newController: UIViewController) {
-        DispatchQueue.main.async { [weak self] in
-            guard let self = self else { return }
-            
-            if let currentController = self.children.last {
-                self.remove(child: currentController)
-            }
-            self.install(child: newController)
+        if let currentController = children.last {
+            remove(child: currentController)
         }
+        install(child: newController)
     }
     
     func install(child: UIViewController) {
-        DispatchQueue.main.async { [weak self] in
-            guard let self = self else { return }
-            
-            self.addChild(child)
-            self.view.addSubview(child.view)
-            
-            child.view.layout.applyConstraint { make in
-                make.constrainEdges(to: self.view)
-            }
-            
-            child.didMove(toParent: self)
+        addChild(child)
+        view.addSubview(child.view)
+        
+        child.view.layout.applyConstraint { make in
+            make.constrainEdges(to: view)
         }
+        
+        child.didMove(toParent: self)
+        
     }
     
     func remove(child: UIViewController) {
-        DispatchQueue.main.async {
-            child.view.removeFromSuperview()
-            child.removeFromParent()
-        }
+        child.view.removeFromSuperview()
+        child.removeFromParent()
     }
 }
