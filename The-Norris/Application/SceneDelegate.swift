91bae60d518fdabdcef209b6c13bbf3d564aa0d8
Erick Borges <ericklborges@gmail.com>
2021-02-16 21:56:40 -0300

    Block first view controller from appearing when running tests

--- The-Norris/Application/SceneDelegate.swift
+++ The-Norris/Application/SceneDelegate.swift
@@ -14,8 +14,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let scene = scene as? UIWindowScene else { return }
+        #if !TESTS
         window = UIWindow(windowScene: scene)
         window?.rootViewController = AppFlowController()
         window?.makeKeyAndVisible()
+        #endif
     }
 }
