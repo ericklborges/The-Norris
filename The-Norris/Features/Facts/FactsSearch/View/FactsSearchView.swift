01530a6b261181334101fa5543acdb54fcc2ce2a
Erick Borges <ericklborges@gmail.com>
2021-02-18 22:01:22 -0300

    Add PastQueryCollectionViewCell to FactsSearchView Collection

--- The-Norris/Features/Facts/FactsSearch/View/FactsSearchView.swift
+++ The-Norris/Features/Facts/FactsSearch/View/FactsSearchView.swift
@@ -22,15 +22,13 @@ final class FactsSearchView: UIView {
         collection.showsVerticalScrollIndicator = false
         collection.showsHorizontalScrollIndicator = false
         collection.register(CategoryCollectionViewCell.self)
+        collection.register(PastQueryCollectionViewCell.self)
         collection.registerSectionHeader(FactsSearchSectionHeader.self)
         return collection
     }()
     
     let flowLayout: UICollectionViewFlowLayout = {
         let flowLayout = UICollectionViewFlowLayout()
-        flowLayout.minimumLineSpacing = 8.0
-        flowLayout.minimumInteritemSpacing = 8.0
-        flowLayout.sectionInset = UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
         flowLayout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
         flowLayout.headerReferenceSize = CGSize(width: UIScreen.main.bounds.width, height: 38)
         return flowLayout
@@ -38,6 +36,7 @@ final class FactsSearchView: UIView {
     
     // MARK: - Properties
     private var categories: [String] = []
+    private var pastQueries: [String] = []
     weak var delegate: FactsSearchViewDelegate?
     
     // MARK: - Life Cycle
@@ -62,9 +61,30 @@ final class FactsSearchView: UIView {
 
 // MARK: - UICollectionViewDataSource
 
-extension FactsSearchView: UICollectionViewDelegate {
+extension FactsSearchView: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        delegate?.didSelectSuggestion(categories[indexPath.row])
+        guard let section = Section(rawValue: indexPath.section) else { return }
+        switch section {
+        case .suggestion:
+            delegate?.didSelectSuggestion(categories[indexPath.row])
+        case .pastQuery:
+            delegate?.didSelectSuggestion(pastQueries[indexPath.row])
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        guard let section = Section(rawValue: section) else { return .zero }
+        return section.insets
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        guard let section = Section(rawValue: section) else { return 0.0 }
+        return section.minimumSpacing
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        guard let section = Section(rawValue: section) else { return 0.0 }
+        return section.minimumSpacing
     }
 }
 
@@ -72,23 +92,50 @@ extension FactsSearchView: UICollectionViewDelegate {
 
 extension FactsSearchView: UICollectionViewDataSource {
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
+        let sections = [!categories.isEmpty, !pastQueries.isEmpty].filter { $0 }
+        return sections.count
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return categories.count
+        guard let section = Section(rawValue: section) else { return 0 }
+        switch section {
+        case .suggestion:
+            return categories.count
+        case .pastQuery:
+            return pastQueries.count
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell: CategoryCollectionViewCell = collectionView.dequeueReusableCell(for: indexPath)
-        cell.setup(category: categories[indexPath.row])
-        return cell
+        guard let section = Section(rawValue: indexPath.section) else { return UICollectionViewCell() }
+        switch section {
+        case .suggestion:
+            let cell: CategoryCollectionViewCell = collectionView.dequeueReusableCell(for: indexPath)
+            cell.setup(category: categories[indexPath.row])
+            return cell
+        case .pastQuery:
+            let cell: PastQueryCollectionViewCell = collectionView.dequeueReusableCell(for: indexPath)
+            cell.setup(query: pastQueries[indexPath.row])
+            return cell
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
         let sectionHeader: FactsSearchSectionHeader = collectionView.dequeueReusableSectionHeader(for: indexPath)
-        sectionHeader.setup(title: "Suggestions")
-        sectionHeader.isHidden = categories.isEmpty
+        
+        guard let section = Section(rawValue: indexPath.section) else {
+            sectionHeader.isHidden = true
+            return sectionHeader
+        }
+        
+        switch section {
+        case .suggestion:
+            sectionHeader.isHidden = categories.isEmpty
+        case .pastQuery:
+            sectionHeader.isHidden = pastQueries.isEmpty
+        }
+        
+        sectionHeader.setup(title: section.title)
         return sectionHeader
     }
 }
@@ -108,5 +155,51 @@ extension FactsSearchView: ViewCodable {
     
     func setupAditionalConfiguration() {
         collectionView.backgroundColor = Color.Background.main
+    }
+}
+
+// MARK: - Section
+
+extension FactsSearchView {
+    
+    enum Section: Int {
+        case suggestion = 0
+        case pastQuery = 1
+        
+        var title: String {
+            switch self {
+            case .suggestion:
+                return "Suggestions"
+            case .pastQuery:
+                return "Past searches"
+            }
+        }
+        
+        var insets: UIEdgeInsets {
+            switch self {
+            case .suggestion:
+                return UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
+            case .pastQuery:
+                return .zero
+            }
+        }
+        
+        var minimumSpacing: CGFloat {
+            switch self {
+            case .suggestion:
+                return 8.0
+            case .pastQuery:
+                return 0.0
+            }
+        }
+        
+        var estimatedSize: CGSize {
+            switch self {
+            case .suggestion:
+                return UICollectionViewFlowLayout.automaticSize
+            case .pastQuery:
+                return CGSize(width: UIScreen.main.bounds.width, height: 44.0)
+            }
+        }
     }
 }
