59e650243298059d4869250c75931a53684a6fd4
Erick Borges <ericklborges@gmail.com>
2021-02-20 13:26:50 -0300

    Add NetworkMonitor

---
+++ Client/NetworkMonitor/NetworkMonitor.swift
@@ -0,0 +1,48 @@
+//
+//  NetworkMonitor.swift
+//  Client
+//
+//  Created by erick.lozano.borges on 20/02/21.
+//
+
+import Network
+
+public class NetworkMonitor {
+    
+    // MARK: - Singleton
+    public static let shared = NetworkMonitor()
+    
+    private init() { }
+    
+    // MARK: - Properties
+    private let monitor = NWPathMonitor()
+    private var currentStatus: NWPath.Status = .requiresConnection
+    
+    var testsIsReachable: () -> Bool = { true }
+    
+    public var isReachable: Bool {
+        #if TESTS
+        return testsIsReachable()
+        #else
+        return currentStatus == .satisfied
+        #endif
+    }
+    
+    // MARK: - Methods
+    public func startMonitoring() {
+        let queue = DispatchQueue(label: "NetworkMonitor")
+        monitor.pathUpdateHandler = { [weak self] path in
+            self?.currentStatus = path.status
+            if path.status == .satisfied {
+                print("[debug] Connected")
+            } else {
+                print("[debug] Disconnected")
+            }
+        }
+        monitor.start(queue: queue)
+    }
+
+    public func stopMonitoring() {
+        monitor.cancel()
+    }
+}
--- The-Norris/Application/AppDelegate.swift
+++ The-Norris/Application/AppDelegate.swift
@@ -5,6 +5,7 @@
 //  Created by erick.lozano.borges on 08/02/21.
 //
 
+import Client
 import UIKit
 
 @main
@@ -13,8 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         #if !TESTS
         Database.shared.loadPersistentStores()
+        NetworkMonitor.shared.startMonitoring()
         #endif
-        
         return true
     }
 
